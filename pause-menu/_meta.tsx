8de2663b4bacc2d3fa599c6f556f46dc4192bb5a
ESCKaybeden <83306334+ESCKaybeden@users.noreply.github.com>
2025-05-31 04:06:53 +0300

    first 2

--- pause-menu/_meta.tsx
+++
@@ -1,9 +0,0 @@
-import type { MetaRecord } from 'nextra'
-
-export default {
-    showcase: 'Showcase',
-    "how-is-it-customize": "How is it Customize ?"
-    // install: 'How to install ?',
-    // customize: 'How is it Customize ?',
-    // changelog: 'Changelog',
-} satisfies MetaRecord
--- pause-menu/how-is-it-customize/_meta.tsx
+++
@@ -1,8 +0,0 @@
-import type { MetaRecord } from 'nextra'
-
-export default {
-    "player-details": 'Player Details',
-    "social-online-time": 'Social/Online/Time',
-    "custom-page": 'Custom Page',
-    "announces": 'Announces'
-} satisfies MetaRecord
